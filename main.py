816f3418971b7d4aacb925f3d192ef6fd9506263
svetika44 <sladkova@google.com>
2016-06-08 17:55:54 -0700

    for avner

--- main.py
+++ main.py
@@ -23,25 +23,30 @@ my_env = jinja2.Environment(
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
 
+def choose_mood (mood): # choose_mood is a function 
+    print 'Your mood is ' +mood  
+    return 'Here is a song ' +mood_dictionary["happy"][1]
+
+mood_dictionary={"happy":["joy by Vivaldi", "Marriage of Figaro by Mozart", "Barber of Seville by Rossini"], "sad":"grief by Mozart"}
+
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+  def get(self):
+      self.response.write('Hello world!')
+  
 
-class OurHandler(webapp2.RequestHandler):
+class OurHandler(webapp2.RequestHandler): # we copied this handler from main one
+  
+  def get(self):
+    mood_template = my_env.get_template('templates/form.html')
+    self.response.write(mood_template.render())
+
   def post(self):
-
-    my_result=self.request.get("user_word")
+    my_result=self.request.get("user_mood") # my_result var stores form value ( form.html)
     print (my_result)
 
-
-
-  def get(self):
-      mood_template = my_env.get_template('templates/form.html')
-
-      self.response.write(mood_template.render())
-
-
-
+    my_dic = {"song_choise":choose_mood(my_result)} # my dictionary choose_mood is a function 
+    music_template = my_env.get_template('templates/results.html')
+    self.response.write(music_template.render(my_dic))
 
 
 app = webapp2.WSGIApplication([
